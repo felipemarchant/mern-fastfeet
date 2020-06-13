cbd2d909c58f4cd8fe3e227f58d24ef409294ab8
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2020-06-13 17:16:07 -0300

    Add Recipient Model

---
+++ backend/src/app/models/Recipient.js
@@ -0,0 +1,16 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Recipient extends Model {
+    static init(sequelize) {
+        super.init({
+            street: Sequelize.STRING,
+            number: Sequelize.INTEGER, 
+            complement: Sequelize.STRING,
+            state: Sequelize.STRING,
+            city: Sequelize.STRING,
+            zip_code: Sequelize.NUMBER
+        }, { sequelize });
+        return this;
+    }
+}
+
