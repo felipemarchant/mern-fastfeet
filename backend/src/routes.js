237a4afd2c63ff94c0064aee9beb86e63f7b60fc
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2020-06-13 18:20:19 -0300

    Adiçao das routes vinculada aos controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,14 @@
 import { Router } from 'express';
 
+import AuthController from './app/controllers/AuthController';
+import RecipientController  from './app/controllers/AuthController';
+
 const routes = new Router();
+
+routes.post('/auth', AuthController.auth);
+
+routes.get('/recipients', RecipientController.index);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
 
 export default routes;
