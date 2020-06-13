d274d6764b5f218666cabc7ebd7c3281a6161d3c
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2020-06-13 16:15:31 -0300

    Adiçao do routes

---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import express from 'express';
+import Youch from 'youch';
+import 'express-async-errors';
+import routes  from './routes';
+import './database';
+import { resolve } from 'path';
+
+class App {
+    constructor() {
+        this.server = express();
+        this.middleware();
+        this.routes();
+        this.exceptionHandler();
+    }
+
+    middleware() {
+        this.server.use(express.json());
+    }
+
+    routes () {
+        this.server.use(routes);
+    }
+
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            let errors = { error: 'Internal server error.' };
+            if (process.env.NODE_ENV === 'development')
+                errors = await new Youch(err, req).toJSON();
+            return res.status(500).json(errors);
+        });
+    }
+}
+
+export default new App().server;
