8a520677c4f7b9489a6bfc42f4077b7649382cbf
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2020-06-19 19:59:14 -0300

    Adiçao da encomenda

---
+++ backend/src/app/models/Order.js
@@ -0,0 +1,16 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Order extends Model {
+    static init(sequelize) {
+        super.init({
+            name: Sequelize.STRING,
+            email: Sequelize.STRING, 
+        }, { sequelize });
+    }
+
+    static associate(models) {
+        this.belongsTo(models.Recipient, { foreignKey: 'recipient_id', as: 'recipient' });
+        this.belongsTo(models.Deliveryman, { foreignKey: 'deliveryman_id', as: 'deliveryman' });
+        this.belongsTo(models.File, { foreignKey: 'signature_id', as: 'signature' });
+    }
+};
