99bd4f4f365fc681c96f44f48eb7be540194f863
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2020-06-19 19:32:56 -0300

    Adiçao de migrations

---
+++ backend/src/app/controllers/DeliverymanController.js
@@ -0,0 +1,9 @@
+class DeliverymanController {
+    async index(req, res) {}
+    async store(req, res) {}
+    async show(req, res) {}
+    async update(req, res) {}
+    async delete(req, res) {}
+}
+
+export default new DeliverymanController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import AuthController from './app/controllers/AuthController';
 import RecipientController  from './app/controllers/RecipientController';
+import DeliverymanController  from './app/controllers/DeliverymanController';
 import auth from './app/middlewares/auth';
 
 const routes = new Router();
@@ -13,5 +14,11 @@ routes.use(auth);
 routes.get('/recipients', RecipientController.index);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+routes.get('/deliveryman', DeliverymanController.index);
+routes.post('/deliveryman', DeliverymanController.store);
+routes.get('/deliveryman/:deliveryman', DeliverymanController.show);
+routes.put('/deliveryman/:deliveryman', DeliverymanController.update);
+routes.put('/deliveryman/:deliveryman', DeliverymanController.delete);
 
 export default routes;
