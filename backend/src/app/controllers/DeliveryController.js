b0496780d180292dfc3ba0b786a003c6845c3266
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2020-06-26 21:22:49 -0300

    Limite de 5 retiradas por dia

--- backend/src/app/controllers/DeliveryController.js
+++ backend/src/app/controllers/DeliveryController.js
@@ -1,5 +1,6 @@
 import Order from '../models/Order';
 import { Op } from 'sequelize';
+import { startOfDay, endOfDay } from 'date-fns';
 
 class DeliveryController {
     async index(req, res) {
@@ -12,6 +13,13 @@ class DeliveryController {
         const { deliveryman: deliveryman_id, delivery: id } = req.params;
         const { start_date, end_date } = req.body;
         const order = await Order.findOne({ where: { id, deliveryman_id, canceled_at: null } });
+        const date = new Date();
+        const limit = start_date ?
+            await Order.count({ where: { id, deliveryman_id, start_date: { [Op.between]: [startOfDay(date), endOfDay(date)] } } }) :
+            0;
+        console.log(limit);
+        if (limit >= 5)
+            return res.status(400).json({ error: 'The Limit is five deliveries per day' });
         let data = start_date ? { start_date: new Date() } : {};
         data = end_date ? { ...data, end_date: new Date() } : data;
         await order.update(data);
